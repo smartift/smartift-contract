dd4cc87180117d7a908f9b8abf1f773bdb77c915
Guy Powell <guy@guytp.org>
2017-06-30 17:17:03 +0100

    Added ECR20 Methods

--- Smart.sol
+++ Smart.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 
 contract AdminManagedContract {
     /* Map our users to admins */
-    mapping (address => bool) public adminUsers;
+    mapping (address => bool) adminUsers;
 
     function AdminManagedContract() {
         adminUsers[msg.sender] = true;
@@ -28,46 +28,100 @@ contract AdminManagedContract {
 
 contract SmartTradingFund is AdminManagedContract {
     /* Map all our our balances for issued tokens */
-    mapping (address => uint256) public smartBalances;
+    mapping (address => uint256) balances;
+
+    /* Map between users and their approval addresses and amounts */
+    mapping(address => mapping (address => uint256)) allowed;
 
     /* The name of the contract */
-    string public name = "Smart Trading Fund";
+    string public constant name = "Smart Trading Fund";
 
     /* The symbol for the contract */
-    string public symbol = "SMRT";
+    string public constant symbol = "SMRT";
 
     /* How many DPs are in use in this contract */
-    uint8 public decimals = 0;
+    uint8 public constant decimals = 0;
 
     /* Defines whether or not we are in the ICO phase */
-    bool public icoPhase = true;
+    bool icoPhase = true;
 
     /* Defines the sale price during ICO */
     uint256 icoUnitPrice = 10 finney;
 
+    uint256 _totalSupply = 0;
+
     /* Our transfer event to fire whenever we shift SMRT around */
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event DebugStr(string message);
-    event DebugInt(uint256 message);
-    event DebugAcct(address message);
+    
+    /* Our approval event when one user approves another to control */
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
 
     /* Initializes contract and adds creator as an admin user */
     function SmartTradingFund() {
     }
 
-    /* Send SMRT to specified recipient */
-    function transfer(address _to, uint256 _value) {
+
+    function totalSupply() constant returns (uint256 totalSupply) {
+        totalSupply = _totalSupply;
+    }
+
+    // What is the balance of a particular account?
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+    // Transfer the balance from owner's account to another account
+    function transfer(address _to, uint256 _amount) returns (bool success) {
         /* Check if sender has balance and for overflows */
-        if (smartBalances[msg.sender] < _value || smartBalances[_to] + _value < smartBalances[_to])
+        if (balances[msg.sender] < _amount || balances[_to] + _amount < balances[_to])
             throw;
 
         /* Add and subtract new balances */
-        smartBalances[msg.sender] -= _value;
-        smartBalances[_to] += _value;
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
 
         /* Fire notification event */
-        Transfer(msg.sender, _to, _value);
+        Transfer(msg.sender, _to, _amount);
+        success = true;
     }
+    
+     // Send _value amount of tokens from address _from to address _to
+     // The transferFrom method is used for a withdraw workflow, allowing contracts to send
+     // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
+     // fees in sub-currencies; the command should fail unless the _from account has
+     // deliberately authorized the sender of the message via some mechanism; we propose
+     // these standardized APIs for approval:
+     function transferFrom(
+         address _from,
+        address _to,
+        uint256 _amount
+    ) returns (bool success) {
+        if (balances[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+            balances[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(_from, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+ 
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
 
     /* Ensures a function can only be called during the ICO */
     modifier preIco {
@@ -99,25 +153,20 @@ contract SmartTradingFund is AdminManagedContract {
         uint256 tokensPurchased = msg.value / icoUnitPrice;
         uint256 purchaseTotalPrice = tokensPurchased * icoUnitPrice;
         uint256 change = msg.value - purchaseTotalPrice;
-        DebugStr("User purchased");
-        DebugInt(tokensPurchased);
-        DebugStr("Change of ");
-        DebugInt(change);
 
         // Increase their new balance if trhey actually purchased any
-        if (tokensPurchased > 0)
-            smartBalances[msg.sender] += tokensPurchased;
+        if (tokensPurchased > 0) {
+            balances[msg.sender] += tokensPurchased;
+            _totalSupply += tokensPurchased;
+        }
 
         // Send change back to recipient
-        if (change > 0) {
-            DebugStr("Sending change back to");
-            DebugAcct(msg.sender);
-            if (!msg.sender.send(change))
+        if (change > 0 && !msg.sender.send(change))
                 throw;
-        }
-        
+
         // Fire transfer event
-        Transfer(0, msg.sender, tokensPurchased);
+        if (tokensPurchased > 0)
+            Transfer(0, msg.sender, tokensPurchased);
     }
 
     function receiveEtherLive() private postIco {
