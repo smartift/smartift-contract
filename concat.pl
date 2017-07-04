caa3dfa6c2d7d113bbbf1a3fe615b98ada9e9430
Guy Powell <guy@guytp.org>
2017-07-04 22:08:56 +0100

    Updated tools to concatenate all sourcecode is a way it can easily be dropped into Ether wallet for one means of test deploy

--- concat.pl
+++ concat.pl
@@ -1,8 +1,16 @@
 #!/usr/bin/perl
 $all = `cat *.sol`;
-$all =~ s/\r//g;
-$all =~ s/\n//g;
-$all =~ s/\t/ /g;
-$all =~ s/( )+/ /g;
-
-print $all;
+$processed =~ s/\r/\n/g;
+@lines = split(/\n/, $all);
+$processed = "";
+foreach (@lines) {
+  if ($_ =~/\\\\/) {
+    print "Cannot process - found double slash comment\n";
+    exit(1);
+  }
+  if (substr($_, 0, 7) ne "import ") {
+    $processed .= $_;
+  }
+}
+$processed =~ s/\r//g;
+print $processed;
