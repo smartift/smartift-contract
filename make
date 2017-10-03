e38af15ca1e5ceef559a63f52523e275ff2240ad
Guy Powell <guy@guytp.org>
2017-10-03 12:17:39 +0100

    Added SVP002 smart contract

--- make
+++ make
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src
 #solc --abi --bin --optimize -o ../out --overwrite SmartInvestmentFundToken.sol DividendManager.sol TransparencyRelayer.sol 
-solc --abi --bin --optimize -o ../out --overwrite VoteSvp001.sol
+solc --abi --bin --optimize -o ../out --overwrite VoteSvp002.sol
 cd ..
 rm out/SafeMath* out/VotingBase* 2>/dev/null
---
+++ src/VoteSvp002.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.4.11;
+import "VotingBase.sol";
+import "SafeMath.sol";
+
+contract VoteSvp001 is VotingBase {
+    using SafeMath for uint256;
+
+    /* Votes for SVP001-01.  0 = not votes, 1 = Yes, 2 = No */
+     mapping (address => uint256) vote01;
+     uint256 public vote01YesCount;
+     uint256 public vote01NoCount;
+
+    /* Votes for SVP001-02.  0 = not votes, 1 = Yes, 2 = No */
+     mapping (address => uint256) vote02;
+     uint256 public vote02YesCount;
+     uint256 public vote02NoCount;
+
+    /* Create our contract with references to other contracts as required. */
+    function VoteSvp001(address _authenticationManagerAddress, uint256 _voteStartTime, uint256 _voteEndTime) {
+        /* Setup access to our other contracts and validate their versions */
+        authenticationManager = AuthenticationManager(_authenticationManagerAddress);
+        if (authenticationManager.contractVersion() != 100201707171503)
+            throw;
+
+        /* Store start/end times */
+        if (_voteStartTime >= _voteEndTime)
+            throw;
+        voteStartTime = _voteStartTime;
+        voteEndTime = _voteEndTime;
+    }
+
+     function voteSvp01(bool vote) {
+        // Forbid outside of voting period
+        if (now < voteStartTime || now > voteEndTime)
+            throw;
+
+         /* Ensure they have voting rights first */
+         uint256 voteWeight = voteCount[msg.sender];
+         if (voteWeight == 0)
+            throw;
+        
+        /* Set their vote */
+        uint256 existingVote = vote01[msg.sender];
+        uint256 newVote = vote ? 1 : 2;
+        if (newVote == existingVote)
+            /* No change so just return */
+            return;
+        vote01[msg.sender] = newVote;
+
+        /* If they had voted previous first decrement previous vote count */
+        if (existingVote == 1)
+            vote01YesCount -= voteWeight;
+        else if (existingVote == 2)
+            vote01NoCount -= voteWeight;
+        if (vote)
+            vote01YesCount += voteWeight;
+        else
+            vote01NoCount += voteWeight;
+     }
+
+     function voteSvp02(bool vote) {
+        // Forbid outside of voting period
+        if (now < voteStartTime || now > voteEndTime)
+            throw;
+
+         /* Ensure they have voting rights first */
+         uint256 voteWeight = voteCount[msg.sender];
+         if (voteWeight == 0)
+            throw;
+        
+        /* Set their vote */
+        uint256 existingVote = vote02[msg.sender];
+        uint256 newVote = vote ? 1 : 2;
+        if (newVote == existingVote)
+            /* No change so just return */
+            return;
+        vote02[msg.sender] = newVote;
+
+        /* If they had voted previous first decrement previous vote count */
+        if (existingVote == 1)
+            vote02YesCount -= voteWeight;
+        else if (existingVote == 2)
+            vote02NoCount -= voteWeight;
+        if (vote)
+            vote02YesCount += voteWeight;
+        else
+            vote02NoCount += voteWeight;
+     }
+}
